1c24f5948b18a476c3444e08ffbb44ce8319d55f
ricmel111 <40599467+ricmel111@users.noreply.github.com>
2025-03-29 15:28:08 +0000

    structured data

--- src/artists.ts
+++ src/artists.ts
@@ -35,7 +35,7 @@ export const artists = [
       website: "https://www.markmccabe.ie/",
     },
     {
-      name: "BWitched",
+      name: "B*Witched",
       image: "/images/artists/BWitched.png",
       website: "https://bwitchedofficial.com/",
     },
@@ -45,7 +45,7 @@ export const artists = [
       website: "https://linktr.ee/garronnoone",
     },
     {
-      name: "Tumbling Paddies",
+      name: "The Tumbling Paddies",
       image: "/images/artists/TumblingPaddies.jpg",
       website: "https://thetumblingpaddiesofficial.com/",
     },
